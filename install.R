fe188f5b06d5cdb573b017e61172f25c6457a504
Nagdev Amruthnath <33914037+nagdevAmruthnath@users.noreply.github.com>
2019-01-14 22:09:59 -0500

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,2 @@
 #install.packages(c("mclust", "e1071", "randomForest", "anytime", "lubridate", "caret","imputeTS", "caret","mlbench","qcc","dplyr","mdatools","parallelDist"))
+install.packages(c("mclust", "anytime", "lubridate", "caret","imputeTS", "caret","mlbench","scatterplot3d"))
